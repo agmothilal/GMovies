1d83b3b31666939c1a6015067cb851da7115f1c8
agmothilal <ag.mothilal@gmail.com>
2021-03-20 15:07:35 -0500

    REFACTOR AC 5 - Adding get movie by title test case and implementation.

--- src/test/java/com/galvanize/GMovies/integration/MoviesControllerTest.java
+++ src/test/java/com/galvanize/GMovies/integration/MoviesControllerTest.java
@@ -1,6 +1,5 @@
 package com.galvanize.GMovies.integration;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.galvanize.GMovies.dto.GMovieDto;
 import org.hamcrest.core.IsNull;
@@ -13,7 +12,6 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -63,7 +61,7 @@ public class MoviesControllerTest {
 
         mockMvc.perform(rb).
                 andExpect(status().isCreated()).
-                andExpect(jsonPath("name").value("Terminator"));
+                andExpect(jsonPath("title").value("Terminator"));
     }
 
     /**
@@ -86,7 +84,7 @@ public class MoviesControllerTest {
         mockMvc.perform(getRequest)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(1))
-                .andExpect(jsonPath("[0].name").value("Terminator"))
+                .andExpect(jsonPath("[0].title").value("Terminator"))
                 .andDo(print());
     }
 
@@ -117,7 +115,7 @@ public class MoviesControllerTest {
 
         mockMvc.perform(getRequest)
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("name").value("Terminator"))
+                .andExpect(jsonPath("title").value("Terminator"))
                 .andDo(print());
         mockMvc.perform(getRequest1)
                 .andExpect(status().isNoContent())
@@ -157,5 +155,32 @@ public class MoviesControllerTest {
                 .andExpect(jsonPath("release").value(2006))
                 .andExpect(jsonPath("description").value("Superman returns to Earth after spending five years in space examining his homeworld Krypton. But he finds things have changed while he was gone, and he must once again prove himself important to the world."))
                 .andExpect(jsonPath("rating").value(IsNull.nullValue()));
+    }
+
+    /**
+     * Given the GMDB has many movies
+     * When I visit GMDB movies with a non-existing title
+     * Then I receive a friendly message that it doesn't exist
+     */
+    @Test
+    public void getMovieByTitleIsNotFoundTest() throws Exception {
+        List<GMovieDto> movieList = Arrays.asList(
+                new GMovieDto("The Avengers","Joss Whedon", "Robert Downey Jr., Chris Evans, Mark Ruffalo, Chris Hemsworth", 2012, "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.", null),
+                new GMovieDto("Superman Returns","Bryan Singer", "Brandon Routh, Kate Bosworth, Kevin Spacey, James Marsden", 2006, "Superman returns to Earth after spending five years in space examining his homeworld Krypton. But he finds things have changed while he was gone, and he must once again prove himself important to the world.", null)
+        );
+
+        RequestBuilder postRequest = post("/v1/gmdb/movies").
+                contentType(MediaType.APPLICATION_JSON).
+                content(objectMapper.writeValueAsString(movieList));
+        mockMvc.perform(postRequest).
+                andExpect(status().isCreated()).
+                andExpect(content().string("added all movies"));
+
+        RequestBuilder getRequest = get("/v1/gmdb/movie?name=Steel")
+                .contentType(MediaType.APPLICATION_JSON);
+
+        mockMvc.perform(getRequest)
+                .andExpect(status().isNoContent())
+                .andExpect(content().string("Movie not found"));
     }
 }
