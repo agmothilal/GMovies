84d7c416caf2ba8a52665a0f49a18479dbf327c1
agmothilal <ag.mothilal@gmail.com>
2021-03-20 09:59:36 -0500

    Initial commit

---
+++ src/main/java/com/galvanize/GMovies/GMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.GMovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GMoviesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galvanize/GMovies/GMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.GMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
