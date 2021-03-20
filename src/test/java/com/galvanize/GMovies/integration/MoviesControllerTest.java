98f039966ac66ef56533c208c0d66ef159db302e
saideepak18 <kodur.saideepak@gmail.com>
2021-03-20 12:54:35 -0400

    RED-AC 4 for adding movies and fetching a movie

--- src/test/java/com/galvanize/GMovies/integration/MoviesControllerTest.java
+++ src/test/java/com/galvanize/GMovies/integration/MoviesControllerTest.java
@@ -1,5 +1,6 @@
 package com.galvanize.GMovies.integration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.galvanize.GMovies.dto.GMovieDto;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -82,5 +87,33 @@ public class MoviesControllerTest {
                 .andExpect(jsonPath("length()").value(1))
                 .andExpect(jsonPath("[0].name").value("Terminator"))
                 .andDo(print());
+    }
+
+    /**
+     * Given the GBDB has many movies
+     * When I visit GMDB movies
+     * Then I should see that movie in GMDB movies
+     */
+    @Test
+    public void checkGMDBMovieTest() throws Exception {
+       List<GMovieDto> movieList =   Arrays.asList(
+               new GMovieDto("Terminator"),
+               new GMovieDto("Jurassic Park"),
+               new GMovieDto("Batman")
+       );
+
+        RequestBuilder postRequest = post("/v1/gmdb/movies").
+                contentType(MediaType.APPLICATION_JSON).
+                content(objectMapper.writeValueAsString(movieList));
+        mockMvc.perform(postRequest).
+                andExpect(status().isCreated());
+
+        RequestBuilder getRequest = get("/v1/gmdb/movie?name=Terminator")
+                .contentType(MediaType.APPLICATION_JSON);
+        mockMvc.perform(getRequest)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("Terminator"))
+                .andDo(print());
+
     }
 }
